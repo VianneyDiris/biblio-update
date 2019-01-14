77dc11b450e62998a9a7ee3dcdd32c915b5a8c44
vianney diris <vianney.diris@gmail.com>
2019-01-14 14:02:27 +0100

    Ajout des objets de base pour le batch notification

---
+++ notification/src/main/java/com/batch/app/notification/BatchTasklet.java
@@ -0,0 +1,20 @@
+package com.batch.app.notification;
+
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BatchTasklet implements Tasklet {
+
+    @Value("${batch.message}") private String message;
+
+    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+        System.out.println(message);
+
+        return RepeatStatus.FINISHED;
+    }
+}
